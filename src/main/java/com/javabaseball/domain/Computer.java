c14d3b6b277c4124fff60201a84c4947ed564541
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2022-04-24 17:21:47 +0900

    [feat]: 볼, 스트라이크 카운터 구현

--- src/main/java/com/javabaseball/domain/Computer.java
+++ src/main/java/com/javabaseball/domain/Computer.java
@@ -1,14 +1,22 @@
 package com.javabaseball.domain;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.javabaseball.type.Constants;
 
 public class Computer {
 	private ArrayList<Integer> generateNumbers;
+	private int strike;
+	private int ball;
 
-	public void generateRandomNumbers() {
+	public void countScore(List<Integer> input) {
+		countStrike(input);
+		countBall(input);
+	}
+
+	private void generateRandomNumbers() {
 		generateNumbers = new ArrayList<>();
 		while (generateNumbers.size() < Constants.BASEBALL_LENGTH) {
 			int num = ThreadLocalRandom.current().nextInt(Constants.MIN_NUM, Constants.MAX_NUM);
@@ -17,4 +25,25 @@ public class Computer {
 			}
 		}
 	}
+
+	private void countStrike(List<Integer> input) {
+		int count = 0;
+		for (int i = 0; i < Constants.BASEBALL_LENGTH; i++) {
+			if (input.get(i).equals(this.generateNumbers.get(i))) {
+				count++;
+			}
+		}
+		this.strike = count;
+	}
+
+	public void countBall(List<Integer> input) {
+		int count = 0;
+		for (int i = 0; i < Constants.BASEBALL_LENGTH; i++) {
+			if (this.generateNumbers.contains(input.get(i))) {
+				count++;
+			}
+		}
+		this.ball = count - this.strike; // ball이 strike랑 중복가능성이 있다.
+	}
+
 }
